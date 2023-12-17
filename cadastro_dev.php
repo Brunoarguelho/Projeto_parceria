c0b4e07a2cd387c366ab43825a527bf04e08fb00
Bruno A. flauzino <bruno1995jf@gmail.com>
2023-12-17 14:48:17 -0300

    add dados iniciais,projeto em andamento

---
+++ cadastro_dev.php
@@ -0,0 +1,11 @@
+
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+// Aqui você pode inserir o código para conectar ao seu banco de dados
+// e inserir os dados do desenvolvedor
+
+echo "Cadastro realizado com sucesso!";
+?>
