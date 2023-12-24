0aed39beae6c4a60d98f18f653c535741709cb73
Bruno A. flauzino <bruno1995jf@gmail.com>
2023-12-24 17:29:46 -0300

    em eveloção

--- site/javascript/site.js
+++ site/javascript/site.js
@@ -1,1 +1,28 @@
-//menu lateral
+document.getElementById('abrir-menu').addEventListener('click', function() {
+    document.querySelector('.menu-lateral').style.transform = 'translateX(0)';
+    document.querySelector('.container').style.filter = 'blur(10px)';
+    // Desabilita a rolagem do mouse
+     //abre menu lateral
+    document.body.style.overflow = 'hidden';
+});
+
+document.getElementById('fechar-menu').addEventListener('click', function() {
+    document.querySelector('.menu-lateral').style.transform = 'translateX(-100%)';
+    document.querySelector('.container').style.filter = 'none';
+    // Habilita a rolagem do mouse
+    //abre menu lateral
+    document.body.style.overflow = 'auto';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target.closest('.menu-lateral') === null && event.target.id !== 'abrir-menu') {
+        document.querySelector('.menu-lateral').style.transform = 'translateX(-100%)';
+        document.querySelector('.container').style.filter = 'none';
+        // Habilita a rolagem do mouse
+         //abre menu lateral
+        document.body.style.overflow = 'auto';
+    }
+});
+
+/*--------------------------------- */
+
